5b17e45cc30d5152a33ad740cbc7be7947534ab0
realespee <wanyamasp@gmail.com>
2023-05-03 10:25:06 +0300

    fetch overtime hours from timesheet to salary slip modify

--- addons/custom_script/salary_slip/salary_slip.js
+++ addons/custom_script/salary_slip/salary_slip.js
@@ -1,21 +1,22 @@
 frappe.ui.form.on('Salary Slip', {
-    onload(frm) {
+    employee(frm) {
        // your code here
- 
-       frappe.call({
-            method: "addons.custom_script.salary_slip.salary_slip.fetch_overtime_hours",
-            args: {
-                'employee': frm.doc.employee, 
-            },
-            callback: function (r) {
-                if (r.message) {
-                    const overtime_hours = r.message
-                    frm.set_value("overtime_hours", overtime_hours)
-                    frm.refresh_field("overtime_hours")
+        if(frm.doc.employee){
+            frappe.call({
+                method: "addons.custom_script.salary_slip.salary_slip.fetch_overtime_hours",
+                args: {
+                    'employee': frm.doc.employee, 
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        const overtime_hours = r.message
+                        frm.set_value("overtime_hours", overtime_hours)
+                        frm.refresh_field("overtime_hours")
+                    }
                 }
-            }
-       });
- 
+           });    
+        }
+       
     }
 
 })
