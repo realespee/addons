6b5e0c1f8cd9773cbeedcefc14fe9024744967f1
realespee <wanyamasp@gmail.com>
2023-05-07 19:05:04 +0300

    compute the cumulative overtime_bonus_rate

--- addons/overrides/salary_slip/salary_slip.py
+++ addons/overrides/salary_slip/salary_slip.py
@@ -24,7 +24,6 @@ class CustomSalarySlip(SalarySlip):
             for data in timesheets:
                 # Edited line below to add overtime hours to timesheet childtable
                 self.append("timesheets", {"time_sheet": data.name, "working_hours": data.total_hours, "overtime_hours": data.overtime_hours})
-            self.overtime_bonus_rate = timesheets[0].project_overtime_rate
     
     def pull_sal_struct(self):
         from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
@@ -35,7 +34,9 @@ class CustomSalarySlip(SalarySlip):
             self.base_hour_rate = flt(self.hour_rate) * flt(self.exchange_rate)
             self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
             # Added line below to add total overtime hours
-            self.total_overtime_hours = sum([d.overtime_hours or 0.0 for d in self.timesheets]) 
+            self.total_overtime_hours = sum([d.overtime_hours or 0.0 for d in self.timesheets])
+            # Added line below to calculate cumulative overtime_bonus_rate
+            self.overtime_bonus_rate = self.overtime_rate(self.total_overtime_hours)
             wages_amount = self.hour_rate * self.total_working_hours
 
             self.add_earning_for_hourly_wages(
@@ -43,3 +44,14 @@ class CustomSalarySlip(SalarySlip):
             )
 
         make_salary_slip(self._salary_structure_doc.name, self)
+
+    def overtime_rate(self, overtime_hours):
+        '''
+        Compute Overtime Bonus Rate, it increases by 0.25 for every 2 more hours of overtime
+        '''
+        if overtime_hours < 2:
+            return 1
+        elif overtime_hours == 4:
+            return 1.25
+        else:
+            return 1.25 + (overtime_hours - 4) // 2 * 0.25
